85fce50c8a08764b26915e30642002f1fd84c711
Adnan-Malik-26 <eddymalikadnan@gmail.com>
2025-03-23 12:56:08 +0530

    Added variables project

---
+++ variable/target/debug/deps/variable-a1a339954e26a5c1.d
@@ -0,0 +1,5 @@
+/home/adnanmalik/code/rust/variable/target/debug/deps/variable-a1a339954e26a5c1: src/main.rs
+
+/home/adnanmalik/code/rust/variable/target/debug/deps/variable-a1a339954e26a5c1.d: src/main.rs
+
+src/main.rs:
---
+++ variable/target/debug/variable.d
@@ -0,0 +1,1 @@
+/home/adnanmalik/code/rust/variable/target/debug/variable: /home/adnanmalik/code/rust/variable/src/main.rs
